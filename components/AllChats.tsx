19fb362af15d014d618437598aad85af986be1d5
Isha736 <ishap736@gmail.com>
2023-03-13 13:14:39 +0000

    added components/chat page

---
+++ components/AllChats.tsx
@@ -0,0 +1,14 @@
+type ChatType = {
+  id: string;
+  name: string;
+};
+
+const AllChats = () => {
+  return (
+    <>
+      <h1>name</h1>
+    </>
+  );
+};
+
+export default AllChats;
---
+++ components/DisplayChat.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const DisplayChat = (props: any) => {
+  return (
+    <>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-bubble">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          facilis consequatur minima quam reprehenderit fugiat minus commodi
+          voluptatibus voluptates ratione quo dicta aut neque explicabo expedita
+          laudantium corrupti deleniti ipsum.
+        </div>
+      </div>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-bubble">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum id
+          incidunt fuga sunt eos placeat similique soluta eaque ea harum commodi
+          nisi expedita rerum sed aspernatur, reprehenderit aperiam quam?
+          Asperiores?
+        </div>
+      </div>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        <div className="chat-bubble">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas
+          qui voluptate corrupti enim maiores dolorem, quaerat cumque minima
+          dolore facere ea perspiciatis provident possimus eaque soluta rerum?
+          Iste, libero.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayChat;
