f307d88dd2568fcb5159b918166cf165885a9dec
Tegid Goodman-Jones <tegidgj@icloud.com>
2023-05-14 23:06:01 +0100

    added temp tasks seed creation

--- pages/api/temp/index.ts
+++ pages/api/temp/index.ts
@@ -7,25 +7,45 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const projects = [
+  const tasks = [
     {
-      name: "Project 1",
-      description: "This is our first project",
-      leaderId: 1,
+      name: "Set up project 1 env",
+      description: "Set up project 1 env",
+      projectId: 1,
+      employeeId: 1,
+      status: "completed",
     },
     {
-      name: "Develop RestFul API",
-      description: "This is our second project, to develop an API",
-      leaderId: 2,
+      name: "Set up project 2 env",
+      description: "Set up project 2 env",
+      projectId: 2,
+      employeeId: 1,
+      status: "completed",
     },
     {
-      name: "Project 3 - The end",
-      description: "This is our third project, the end",
-      leaderId: 3,
+      name: "start the beginning of the end",
+      description: "start the beginning of the end desc",
+      projectId: 3,
+      employeeId: 1,
+      status: "completed",
+    },
+    {
+      name: "Do some admin",
+      description: "Do some admin desc",
+      projectId: 1,
+      employeeId: 2,
+      status: "inProgress",
+    },
+    {
+      name: "Do some admin",
+      description: "Do some admin desc",
+      projectId: 2,
+      employeeId: 2,
+      status: "inProgress",
     },
   ];
-  const createdProjects = await prisma.project.createMany({
-    data: projects,
-  })
-  res.status(200).json({ success: true, data: createdProjects });
+  const tasksPrisma = await prisma.task.createMany({
+    data: tasks,
+  });
+  res.status(200).json({ success: true, data: tasksPrisma });
 }
