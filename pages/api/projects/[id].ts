d87eb898f962578a9f5e328caf63e8c76d994df5
Tegid Goodman-Jones <tegidgj@icloud.com>
2023-05-15 15:14:41 +0100

    added delete method to projects

--- pages/api/projects/[id].ts
+++ pages/api/projects/[id].ts
@@ -5,24 +5,36 @@ import { decode, verify } from "jsonwebtoken";
 const prisma = new PrismaClient();
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    if (req.method != "GET"){ 
-        return res.status(400).json({ success: false, message: "Invalid request" });
-    }else{
-      try {
-        
-        let { id } = req.query;
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method == "GET") {
+    try {
+      let { id } = req.query;
 
-          const projects = await prisma.project.findMany({
-            where: {
-              id: Number(id),
-            },
-          });
-        res.status(200).json({ success: true, data: projects });
-      } catch (error) {
-        res.status(400).json({ success: false, error: error });
-      }
+      const projects = await prisma.project.findMany({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.status(200).json({ success: true, data: projects });
+    } catch (error) {
+      res.status(400).json({ success: false, error: error });
     }
+  } else if (req.method == "DELETE") {
+    try {
+      let { id } = req.query;
+
+      const projects = await prisma.project.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.status(200).json({ success: true, data: projects });
+    } catch (error) {
+      res.status(400).json({ success: false, error: error });
+    }
+  } else {
+    return res.status(400).json({ success: false, message: "Invalid request" });
   }
+}
